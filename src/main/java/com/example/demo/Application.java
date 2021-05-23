f8b78a34aa00b8ab9402ebf83fcf286a72664cb9
Pramod Mali <pramod.mali44@gmail.com>
2021-05-24 05:26:02 +0530

    Initial Project Commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		run.close();
+		System.out.println("Welcome To Spring Boot ");
 	}
 
 }
