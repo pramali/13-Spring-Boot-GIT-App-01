3656e37f887022eb81b0afe3ba4c3ed6d1c89c21
Pramod Mali <pramod.mali44@gmail.com>
2021-05-26 03:15:08 +0530

    To Get conflict code practise

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
-		
 		private static HashMap map=new HashMap();
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		run.close();
