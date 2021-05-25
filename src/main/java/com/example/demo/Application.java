2b5cd56c0dbc93510b95c8c3de5389a0b8b0d817
Pramod Mali <pramod.mali44@gmail.com>
2021-05-26 02:57:45 +0530

    Initial changes

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
+		private static String="Welcome to GIT";
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		run.close();
 		System.out.println("Welcome To Spring Boot ");
